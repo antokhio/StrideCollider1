9bf81ba3d583036bd18bb376d38ed58053883980
@antokhio <antokhio@gmail.com>
2020-10-19 22:16:02 +0300

    init

---
+++ StrideCollider1.Windows/StrideCollider1App.cs
@@ -0,0 +1,15 @@
+using Stride.Engine;
+
+namespace StrideCollider1
+{
+    class StrideCollider1App
+    {
+        static void Main(string[] args)
+        {
+            using (var game = new Game())
+            {
+                game.Run();
+            }
+        }
+    }
+}
